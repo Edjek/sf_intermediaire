a521672b3ed9c446c496f7020f3cd7e07a138c34
Edjek <edjek@hotmail.fr>
2022-01-12 09:48:32 +0100

    Upload pour update media

--- src/Controller/Admin/AdminMediaController.php
+++ src/Controller/Admin/AdminMediaController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Media;
 use App\Form\MediaType;
+use App\Repository\MediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +13,64 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminMediaController extends AbstractController
 {
+    #[Route('/admin/update/media/{id}', name: 'admin_update_media')]
+    public function updateMedia(
+        $id,
+        MediaRepository $mediaRepository,
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        SluggerInterface $sluggerInterface
+    ) {
+
+        $media = $mediaRepository->find($id);
+
+        $mediaForm = $this->createForm(MediaType::class, $media);
+
+        $mediaForm->handleRequest($request);
+
+        if ($mediaForm->isSubmitted() && $mediaForm->isValid()) {
+            // On récupère le fichier
+            $mediaFile = $mediaForm->get('src')->getData();
+
+            if ($mediaFile) {
+
+                // On créée un nom unique à notre fichier à partir du nom original
+                // Pour éviter tout problème de confusion
+
+                // On récupère le nom original du fichier
+                $originalFilename = pathinfo($mediaFile->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // On utilise slug sur le nom original pour avoir un nom valide du fichier
+                $safeFilename = $sluggerInterface->slug($originalFilename);
+
+                // On ajoute un id unique au nom de l'image
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $mediaFile->guessExtension();
+
+                // On déplace le fichier dans le dossier public/media
+                // la destination du fichier est enregistré dans 'images_directory'
+                // qui est défini dans le fichier config\services.yaml
+
+                $mediaFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+
+                $media->setSrc($newFilename);
+            }
+            $entityManagerInterface->persist($media);
+            $entityManagerInterface->flush();
+
+            $this->addFlash(
+                'notice',
+                'L\'image a été modifié'
+            );
+
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render("admin/admin_media/mediaform.html.twig", ['mediaForm' => $mediaForm->createView()]);
+    }
+
     #[Route('admin/create/media', name: 'admin_create_media')]
     public function createMedia(EntityManagerInterface $entityManagerInterface, Request $request,  SluggerInterface $sluggerInterface)
     {
